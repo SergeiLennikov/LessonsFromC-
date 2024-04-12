6cbacb8a02764a735dd05933a4c40168b1c2462c
Sergei Lennikov <lennikov99@list.ru>
2024-04-12 21:39:23 +0700

    Работа с переменными

---
+++ Lesson1/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Много строчный коментарий
+*/
+// using System.Globalization;
+
+// string str = "1.4";
+
+// // В этой функции мы изменяем числовой формат с , на .
+// NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+// {
+//     NumberDecimalSeparator = ".",
+// };
+
+// double a = double.Parse(str, numberFormatInfo);
+
+// System.Console.WriteLine(a);
+// Console.Read();
+
+
+
+// найдите средне арефметическое число
+
+// System.Console.Write("Введите число ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// double result = (double)(number1 + number2 + number3) / 3;
+
+// System.Console.WriteLine(result);
+
+
+
+//Введите три чесла и выведите на экран значение суммы и произведения этих чисел
+
+// System.Console.Write("Введите число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите число ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// double result2 = (double)num1 * num2 * num3;
+
+// System.Console.WriteLine($"Ответ: {result2}");
+
+
+//Конверт валют
+
+// int var, summ1;
+
+// System.Console.Write("Добро пожаловать в конвертацию валюты!\n");
+// System.Console.Write("На даный момент курс 1 USD = 93.72.\n");
+// System.Console.Write("Хотите Расчитать? \nВыберите вариант:\n1.Доллар к Рублю\n2.Рубль к доллару.\n");
+// var = Convert.ToInt32(Console.ReadLine());
+// if(var == 1)
+// {
+//     Console.Write("Введите сумму в Долларах: ");
+//     summ1 = Convert.ToInt32(Console.ReadLine());
+//     double result1 = (double)summ1 * 93.72;
+//     System.Console.WriteLine($"Доллар к рублю равен: {result1}");
+// }
+// if(var == 2)
+// {
+//     Console.Write("Введите сумму в Рублях: ");
+//     summ1 = Convert.ToInt32(Console.ReadLine());
+//     double result2 = (double)summ1 * 0.01067;
+//     System.Console.WriteLine($"Рубль к доллару равен: {result2}");
+// }
+
