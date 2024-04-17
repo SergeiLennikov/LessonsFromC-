c1ec7bd620039bb660b8f667a12d11aec036e9e4
Sergei Lennikov <lennikov99@list.ru>
2024-04-17 16:33:53 +0700

    Создали счетчик четных и нечетных чисел

--- Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
+++ Lesson3/obj/Debug/net8.0/Lesson3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lesson1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1008157d50b2a796de05e9285a9b42e2fe6896")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lesson1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lesson1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lesson3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lesson3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
