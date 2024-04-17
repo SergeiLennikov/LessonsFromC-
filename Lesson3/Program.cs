ae46c5390b5d96ee05f433d92aef6770c64afeb2
Sergei Lennikov <lennikov99@list.ru>
2024-04-17 16:33:53 +0700

    Создали счетчик четных и нечетных чисел

---
+++ Lesson3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Счетчик четных & нечетных чисел в диапозоне заданным пользователем
+uint oddNumbersCount = 0; //нечетные числа
+uint evenNumbersCount = 0; //четные числа
+
+System.Console.WriteLine("Введите первое число диапозона ");
+int currentValue = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите последние число диапозона ");
+int limit = int.Parse(Console.ReadLine());
+
+while (currentValue <= limit)
+{
+    if (currentValue % 2 == 0)
+    {
+        evenNumbersCount++;
+    }
+    else
+    {
+        oddNumbersCount++;
+    }
+    currentValue++;
+}
+System.Console.WriteLine("Кол-во нечетных чисел: " + oddNumbersCount);
+System.Console.WriteLine("Кол-во четных чисел: " + evenNumbersCount);
