83aa460723b8df38c22b76a8a4bfb7ea082e7176
Sergei Lennikov <lennikov99@list.ru>
2024-04-17 16:31:57 +0700

    Создали счетчик четных и нечетных чисел

--- Lesson2/obj/Debug/net8.0/Lesson2.AssemblyInfo.cs
+++ Lesson2/obj/Debug/net8.0/Lesson2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbacb8a02764a735dd05933a4c40168b1c2462c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1008157d50b2a796de05e9285a9b42e2fe6896")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
