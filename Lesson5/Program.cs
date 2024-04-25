a86d54a86d7d04325f105f22ee057e40fa537ee7
Sergei Lennikov <lennikov99@list.ru>
2024-04-25 19:04:59 +0700

    Тернарный оператор

---
+++ Lesson5/Program.cs
@@ -0,0 +1,24 @@
+﻿// bool accessallowed;
+
+// string storedPassword = "12345";
+// string enteredPassword = Console.ReadLine();
+
+// if (enteredPassword == storedPassword)
+// {
+//     accessallowed = true;
+//     Console.WriteLine("От куда ты узнал пароль ?");
+// }
+// else
+// {
+//     accessallowed = false;
+//     Console.WriteLine("Долбан иди в писку !!!");
+// }
+// Console.WriteLine(accessallowed);
+
+// accessallowed = enteredPassword == storedPassword ? true : false;
+// Console.WriteLine(accessallowed);
+
+int inputData = int.Parse(Console.ReadLine());
+
+int outputData = inputData < 0 ? 0 : inputData;
+System.Console.WriteLine(outputData);
