07379e5cd049a0636d785f983108f81cd5d19020
Sergei Lennikov <lennikov99@list.ru>
2024-04-30 16:10:45 +0700

    Легкие задачки с Массивами

---
+++ Lesson6/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Ввод массива с клавиатуры.
+        // Console.Write("Введите кол-во элементов массива:\t");
+        // int elementsCount = int.Parse(Console.ReadLine());
+        // int[] myArray = new int[elementsCount];
+        
+        // for (int i = 0; i < myArray.Length; i++)
+        // {
+        //     Console.Write($"\nВведите элемент массива под индексом {i}:\t");
+        //     myArray[i] = int.Parse(Console.ReadLine());
+        // }
+
+        // Console.WriteLine("Вывод массива:");
+        // for (int i = 0; i < myArray.Length; i++)
+        // {
+        //     Console.WriteLine($"\n{myArray[i]}");
+        // }
+
+
+        //Вывод массива с индексом.
+        // int[] imArray = { 1, 24, 33, 4, 52, 61};
+        // for (int i = 0; i < imArray.Length; i++)
+        // {
+        //     Console.WriteLine($" {i} => {imArray[i]}");
+        // }
+
+
+        //Вывод массива в обратном порядке.
+        // int[] imArray = { 1, 24, 33, 4, 52, 61};
+        // for (int i = imArray.Length - 1; i >= 0; i--)
+        // {
+        //     Console.WriteLine($" {i} => {imArray[i]}");
+        // }
+
+
+        //Сумма четных элементов массива.
+        // int[] uArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // int sum = 0;
+        // for (int i = 0; i < uArray.Length; i++)
+        // {
+        //     if (uArray[i] % 2 == 0)
+        //     {
+        //         sum += uArray[i];
+        //     }
+        // }
+        // System.Console.WriteLine(sum);
+
+        //Минимальный элемент массива.
+        // int[] tArray = { 12, 22, 34, 46, 5, 66};
+        // int minSum = tArray[0];
+
+        // for (int i = 1; i < tArray.Length; i++)
+        // {
+        //     if (tArray[i] < minSum)
+        //     {
+        //         minSum = tArray[i];
+        //     }  
+        // }
+        // System.Console.WriteLine(minSum);
+    }
+}
