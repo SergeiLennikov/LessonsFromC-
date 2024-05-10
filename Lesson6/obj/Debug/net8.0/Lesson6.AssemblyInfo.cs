7e14e1aa85183be68d2ad6d7de57ebdadb5d1a69
Sergei Lennikov <lennikov99@list.ru>
2024-05-10 15:41:22 +0700

    Работы с одномерными массивами

--- Lesson6/obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
+++ Lesson6/obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07379e5cd049a0636d785f983108f81cd5d19020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b974a6c6d8c125641593677b13fc9653ae6c2ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
